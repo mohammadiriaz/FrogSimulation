fcd71a1635c29edb418f9410471632ca6b382271
mohammadiriaz <mohammadr95@nycstudents.net>
2025-11-14 12:33:25 -0500

    Update FrogSimulation.java

--- src/FrogSimulation.java
+++ src/FrogSimulation.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+public class FrogSimulation
+{
+    private int goalDistance;
 
-public class FrogSimulation {
+    private int maxHops;
+
+    public FrogSimulation(int dist, int numHops)
+    {
+        goalDistance = dist;
+        maxHops = numHops;
+    }
+
+    private int hopDistance()
+    {
+
+    }
+
+
+    public boolean simulate()
+    {
+
+    }
+
+
+    public double runSimulations(int num)
+    {
+
+    }
 }
